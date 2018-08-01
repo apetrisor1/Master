be9667fdae025458061f0f37ee9c54e69e36e8c0
adrian.petrisor <adrian.petrisor@outlook.com>
2018-08-01 13:34:57 -0400

    minor styling on rock paper scissors

--- Portfolio/external/rockPaperScissors/js/script.js
+++ Portfolio/external/rockPaperScissors/js/script.js
@@ -3,6 +3,7 @@
   
 // Call function when DOM is loaded to initiate game.
 document.addEventListener("DOMContentLoaded", function(){
+
 console.log("Rock Paper Scissors");
 console.log("");
 
@@ -26,27 +27,22 @@ console.log("");
   return Math.floor(Math.random() * Math.floor(max));
   }
   
-  // Turns all choices to opaque a short while after choice
-  function turnOpaque(){
-     setTimeout(function() {document.getElementsByClassName('opaque1')[0].style.opacity = "0.2";}, 2250);
-     setTimeout(function() {document.getElementsByClassName('opaque2')[0].style.opacity = "0.2";}, 2250);
-     setTimeout(function() {document.getElementsByClassName('opaque3')[0].style.opacity = "0.2";}, 2250);
-     $('.opaque1').mouseenter(function() {document.getElementsByClassName('opaque1')[0].style.opacity = "0.2";});
-  }
+
   
-  // Choices available to player, mapped out in opaque styling
-  var imagePlayerOpaque1 = document.createElement('playerPic');
-  var imagePlayerOpaque2 = document.createElement('playerPic');
-  var imagePlayerOpaque3 = document.createElement('playerPic');  
-  imagePlayerOpaque1.innerHTML = "<img src = 'assets/images/rock.jpg'>";
-  imagePlayerOpaque2.innerHTML = "<img src = 'assets/images/paper.png'>";
-  imagePlayerOpaque3.innerHTML = "<img src = 'assets/images/scissors.png'>";
-  imagePlayerOpaque1.className = "opaque1";
-  imagePlayerOpaque2.className = "opaque2";
-  imagePlayerOpaque3.className = "opaque3";
-  document.getElementById('container').appendChild(imagePlayerOpaque1);
-  document.getElementById('container').appendChild(imagePlayerOpaque2);
-  document.getElementById('container').appendChild(imagePlayerOpaque3);
+  // Choices available to player
+  var rock = document.createElement('playerPic');
+  var paper = document.createElement('playerPic');
+  var scissors = document.createElement('playerPic');  
+  rock.innerHTML = "<img src='assets/images/rock.jpg'>";
+  paper.innerHTML = "<img src='assets/images/paper.png'>";
+  scissors.innerHTML = "<img src='assets/images/scissors.png'>";
+  rock.className = "rock";
+  paper.className = "paper";
+  scissors.className = "scissors";
+  document.getElementById('container').appendChild(rock);
+  document.getElementById('container').appendChild(paper);
+  document.getElementById('container').appendChild(scissors);
+  
     
   // Player is initially assigned an empty hand
   var newHandPlayer = document.createElement('h3');
@@ -57,88 +53,72 @@ console.log("");
     
   // ROCK
   // Empty hand is populated with the choice of rock and appended to the div with id 'choices'
-  document.getElementsByClassName("opaque1")[0].addEventListener('click', function(){
+  document.getElementsByClassName("rock")[0].addEventListener('click', function(){
      console.log("You have chosen rock!");
      myStats = rollRock;
      newHandPlayer.innerHTML = '';
      newHandPlayer.innerHTML = "Player has picked rock!";
      document.getElementById('choices').appendChild(newHandPlayer);
-     // Visually represent player's choice by turning choice into non-opaque
-     document.getElementsByClassName('opaque1')[0].style.opacity = "1";
-     document.getElementsByClassName('opaque2')[0].style.opacity = "0.2";
-     document.getElementsByClassName('opaque3')[0].style.opacity = "0.2";
-     // choices turn opaque after a short while
-     turnOpaque();
-     
-
+    
     
     // If no pick was made already, we play game and populate screen
      if(choiceMade === false)
        {
-        document.getElementsByClassName("opaque1")[0].addEventListener('click', playGame());
+        document.getElementsByClassName("rock")[0].addEventListener('click', playGame());
         document.getElementById('chooseWeapon').style.visibility = 'hidden';
         document.getElementById('refresh').style.visibility = 'visible';
        }else{ // if playing by choosing a new hand we must first delete previous notifications by removing the respective DOM node.
               // Note that we don't need to alter the visibility of bottom messages because they are set on first playthrough
         document.getElementById('choices').removeChild(document.getElementById('choices').childNodes[1]);
         document.getElementById('result').removeChild(document.getElementById('result').childNodes[1]);
-        document.getElementsByClassName("opaque1")[0].addEventListener('click', playGame());
+        document.getElementsByClassName("rock")[0].addEventListener('click', playGame());
        }
      choiceMade = true; 
   });
     
   // PAPER  
   // Empty hand is populated by choosing Paper and appended to the div with id 'choices'
-  document.getElementsByClassName("opaque2")[0].addEventListener('click',function(){
+  document.getElementsByClassName("paper")[0].addEventListener('click',function(){
        console.log("You have chosen paper!");
        myStats = rollPaper;
        newHandPlayer.innerHTML = '';
        newHandPlayer.innerHTML = "Player has picked paper!";
        document.getElementById('choices').appendChild(newHandPlayer);
-       // Visually represent player's choice by turning choice into non-opaque
-       document.getElementsByClassName('opaque2')[0].style.opacity = "1";
-       document.getElementsByClassName('opaque1')[0].style.opacity = "0.2";
-       document.getElementsByClassName('opaque3')[0].style.opacity = "0.2";
-       turnOpaque();
     
        // If no pick was made already, we play game and populate screen
        if(choiceMade === false){
-         document.getElementsByClassName("opaque2")[0].addEventListener('click', playGame());
+         document.getElementsByClassName("paper")[0].addEventListener('click', playGame());
          document.getElementById('chooseWeapon').style.visibility = 'hidden';
          document.getElementById('refresh').style.visibility = 'visible';
        }else{ // if playing by choosing a new hand we must first delete previous notifications by removing the respective DOM node.
               // Note that we don't need to alter the visibility of bottom messages because they are set on first playthrough
          document.getElementById('choices').removeChild(document.getElementById('choices').childNodes[1]);
          document.getElementById('result').removeChild(document.getElementById('result').childNodes[1]);
-         document.getElementsByClassName("opaque2")[0].addEventListener('click', playGame());
+         document.getElementsByClassName("paper")[0].addEventListener('click', playGame());
        }
        choiceMade = true; 
   });  
     
   // SCISSORS
   // Empty hand is populated by choosing Scissors and appended to the div with id 'choices'
-  document.getElementsByClassName("opaque3")[0].addEventListener('click',function(){
+  document.getElementsByClassName("scissors")[0].addEventListener('click',function(){
        console.log("You have chosen scissors!");
        myStats = rollScissors;
        newHandPlayer.innerHTML = '';
        newHandPlayer.innerHTML = "Player has picked scissors!";
        document.getElementById('choices').appendChild(newHandPlayer);
-       // Visually represent player's choice by turning choice into non-opaque
-       document.getElementsByClassName('opaque3')[0].style.opacity = "1";
-       document.getElementsByClassName('opaque1')[0].style.opacity = "0.2";
-       document.getElementsByClassName('opaque2')[0].style.opacity = "0.2";
-       turnOpaque();
+      
     
        // If no pick was made already, we play game and populate screen
        if(choiceMade === false){
-         document.getElementsByClassName("opaque3")[0].addEventListener('click', playGame());
+         document.getElementsByClassName("scissors")[0].addEventListener('click', playGame());
          document.getElementById('chooseWeapon').style.visibility = 'hidden';
          document.getElementById('refresh').style.visibility = 'visible';
        }else{ // if playing by choosing a new hand we must first delete previous notifications by removing the respective DOM node.
               // Note that we don't need to alter the visibility of bottom messages because they are set on first playthrough
          document.getElementById('choices').removeChild(document.getElementById('choices').childNodes[1]);
          document.getElementById('result').removeChild(document.getElementById('result').childNodes[1]);
-         document.getElementsByClassName("opaque3")[0].addEventListener('click', playGame());
+         document.getElementsByClassName("scissors")[0].addEventListener('click', playGame());
        }
        choiceMade = true; 
   });
@@ -150,17 +130,32 @@ console.log("");
   // B. interprets game-rules and shows result
     
   function playGame(){
-    
-  // A:
+
+    // A:
     
   // Assigns computer an integer. (random int, 0 for Paper, 1 for Scissors, 2 for Rock)
   var computerRoll = getRandomInt(3);
     
   // Computer is assigned an empty hand
   var newHandComputer = document.createElement('h3');
-  newHandComputer.className = ('computer-pick');  
+  newHandComputer.className = ('computer-hand');  
   var imageComputer = document.createElement('computerPic');
   
+    
+    
+    // Using populateComputerHand in cases      0 1 and 2    below achieves the same result, should be better BUT
+    // when clicking to switch hands really fast it fails to show the correct image . 
+    
+//   function populateComputerHand(choice) {
+//         newHandComputer.innerHTML = '';
+//         newHandComputer.innerHTML = "Computer has picked " + choice +"!";
+//         document.getElementById('choices').appendChild(newHandComputer);
+//         imageComputer.innerHTML = `<img src = 'assets/images/${choice}.png'>`;
+//         imageComputer.className = choice + "PC";
+//         document.getElementById('container').appendChild(imageComputer);
+//   }
+    
+    
   // Hand is populated
   switch(computerRoll) {
     case 0:
@@ -170,17 +165,17 @@ console.log("");
         newHandComputer.innerHTML = "Computer has picked paper!";
         document.getElementById('choices').appendChild(newHandComputer);
         imageComputer.innerHTML = "<img src = 'assets/images/paper.png'>";
-        imageComputer.className = "paper2";
+        imageComputer.className = "paperPC";
         document.getElementById('container').appendChild(imageComputer);
         break;
     case 1:
         console.log("Computer has chosen scissors!");
+        computerStats = rollScissors;
         newHandComputer.innerHTML = '';
         newHandComputer.innerHTML = "Computer has picked scissors!";
         document.getElementById('choices').appendChild(newHandComputer);
-        computerStats = rollScissors;
         imageComputer.innerHTML = "<img src = 'assets/images/scissors.png'>";
-        imageComputer.className = "scissors2";
+        imageComputer.className = "scissorsPC";
         document.getElementById('container').appendChild(imageComputer);
         break;
     case 2:
@@ -190,7 +185,7 @@ console.log("");
         newHandComputer.innerHTML = "Computer has picked rock!";
         document.getElementById('choices').appendChild(newHandComputer);
         imageComputer.innerHTML = "<img src = 'assets/images/rock.jpg'>";
-        imageComputer.className = "rock2";
+        imageComputer.className = "rockPC";
         document.getElementById('container').appendChild(imageComputer);
         break;
     default:
@@ -210,6 +205,7 @@ console.log("");
               console.log("Computer wins!");
               result.innerText = "Computer wins!";
               document.getElementById('result').appendChild(result);
+              
       }
       else if(computerStats === rollRock)
         {
@@ -262,7 +258,8 @@ console.log("");
           document.getElementById('result').appendChild(result);
          }
   } 
-  
+ document.getElementsByTagName('span')[0].addEventListener('click', function() {location.reload()});
+
   // 3. Shows in csonsole pseudo distribution of getRandomInt
   // Keeper holds the outcomes of 100 random hands
   
